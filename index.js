f5922a2791c835ac5d6c238dfad7d8bece86796d
Joe Andaverde <joe.andaverde@gmail.com>
2017-09-20 20:35:29 -0500

    Chicken patty wednesday!

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-'use strict';
+'use strict'
 
-const Alexa = require('alexa-sdk');
-const Moment = require('moment-timezone');
-const _ = require('lodash');
+const Alexa = require('alexa-sdk')
+const Moment = require('moment-timezone')
+const _ = require('lodash')
+const LunchMenu = require('./lunch_menu.json')
 
 const handlers = {
    'menu_today': function () {
       const today = Moment.tz('America/Chicago').format('YYYY-MM-DD')
-      this.emit(':tell', 'today is chicken patty wednesday' + today);
+      this.emit(':tell', 'today is chicken patty wednesday' + today)
    },
    'menu_date': function () {
       console.log(this.event.request.intent.slots.time_utterance.value)
       this.emit(':tell', 'red bottom shoes ' + this.event.request.intent.slots.time_utterance.value)
    },
    'AMAZON.HelpIntent': function () {
-      const speechOutput = this.t('HELP_MESSAGE');
-      const reprompt = this.t('HELP_MESSAGE');
-      this.emit(':ask', speechOutput, reprompt);
+      const speechOutput = this.t('HELP_MESSAGE')
+      const reprompt = this.t('HELP_MESSAGE')
+      this.emit(':ask', speechOutput, reprompt)
    },
    'AMAZON.CancelIntent': function () {
-      this.emit(':tell', this.t('STOP_MESSAGE'));
+      this.emit(':tell', this.t('STOP_MESSAGE'))
    },
    'AMAZON.StopIntent': function () {
-      this.emit(':tell', this.t('STOP_MESSAGE'));
+      this.emit(':tell', this.t('STOP_MESSAGE'))
    },
-};
+}
 
 exports.handler = function (event, context) {
-   const alexa = Alexa.handler(event, context);
-   alexa.registerHandlers(handlers);
-   alexa.execute();
-};
+   const alexa = Alexa.handler(event, context)
+   alexa.registerHandlers(handlers)
+   alexa.execute()
+}
